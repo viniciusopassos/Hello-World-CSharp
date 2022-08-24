b189f9885a3561c222d8e944a99bf41da4c30e11
viniciusOP <viniciusopassos@hotmail.com>
2022-08-24 14:34:00 +0000

    correcao

--- src/ola-mundo.Test/OlaMundoTest.cs
+++ src/ola-mundo.Test/OlaMundoTest.cs
@@ -1,15 +1,27 @@
 using ola_mundo;
 using System.IO;
 using System;
+using Xunit;
+using FluentAssertions;
 
 namespace ola_mundo.Test;
 
 public class OlaMundoTest
 {
     [Theory(DisplayName = "Deve exibir Olá, Mundo!")]
-    [InlineData("<Insir algo aqui>")]
+    [InlineData("Olá, Mundo!")]
     public void TestOlaMundo(string Expected)
     {        
-        throw new notImplementedException();                    
+        using (var NewOutput = new StringWriter())
+        
+        {
+            Console.SetOut(NewOutput);
+        
+            Program.Main();
+        
+            string result = NewOutput.ToString().Trim();
+    
+            result.Should().Be(Expected);      
+        }                    
     }
 }
